28c16d3887ad8dfb153f3bda4120c3adb9bc6265
endy <admin@site15.ru>
2017-09-07 08:28:42 +0500

    Fix travis #2

--- scripts/install.sh
+++ scripts/install.sh
@@ -2,10 +2,6 @@ wget https://raw.githubusercontent.com/creationix/nvm/v0.31.0/nvm.sh -O ~/.nvm/n
 source ~/.nvm/nvm.sh
 nvm install 6
 node --version
-add-apt-repository ppa:git-core/ppa -y
-apt-get update
-apt-get install git -y
-pip install --upgrade pip
 pip install -r requirements.txt
 cd frontend
 npm install
